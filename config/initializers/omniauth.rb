de32eca29d5de484181022c3f7d1e6ca79fbb32f
Teresa Martyny <teresamartyny@gmail.com>
2015-06-19 14:23:46 -0700

    change variables to match names from paypal

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :paypal, ENV['APP_ID'], ENV['APP_TOKEN'], sandbox: true, scope: "openid profile email"
+  provider :paypal, ENV['CLIENT_ID'], ENV['SECRET'], sandbox: true, scope: "openid profile email"
 end
 
 # Code below is for testing mode in case live authentication is lagging during testing.

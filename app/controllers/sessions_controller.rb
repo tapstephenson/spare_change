52a326fe5d725276da4b46a2dd0575beb6153e58
Teresa Martyny <teresamartyny@gmail.com>
2015-06-19 15:37:55 -0700

    modify controller per guidelines in setting up omniauth paypal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,25 @@
 class SessionsController < ApplicationController
+  def index
+  end
   def create
-    auth_hash = request.env['omniauth.auth']
-    p "*"*90
-    p auth_hash
-    uid = auth_hash.uid
-    profile = auth_hash.profile
-    email = auth_hash.info.email
 
-    user = User.where(uid: uid).first_or_create
+    user = User.where(uid: auth_hash.uid).first_or_create!(name: auth_hash.info.name, email: auth_hash.info.email)
     session[:user_id] = user.id
     redirect_to :root
   end
 
   def show
     @user = User.find_by_id(session[:user_id])
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :root
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end

7a0ab41b8652ca1c5aae4e42ac238ff7f5b1d7b9
Teresa Martyny <teresamartyny@gmail.com>
2015-06-19 12:18:17 -0700

    add show controller method. modify create controller method to get profile and email from auth hash.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
   def create
-    uid = request.env['omniauth.auth'].uid
+    auth_hash = request.env['omniauth.auth']
+    p "*"*90
+    p auth_hash
+    uid = auth_hash.uid
+    profile = auth_hash.profile
+    email = auth_hash.info.email
+
     user = User.where(uid: uid).first_or_create
     session[:user_id] = user.id
     redirect_to :root
+  end
+
+  def show
+    @user = User.find_by_id(session[:user_id])
   end
 end

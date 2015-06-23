4a57c771f5c50d4da714801d62bb7b97b8b9af41
Teresa Martyny <teresamartyny@gmail.com>
2015-06-23 16:58:17 -0700

    add unfinished_signup method

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,16 @@
 class SettingsController < ApplicationController
+  include UsersConcerns
+
   def index
     render :index
   end
+
+  def unfinished_signup
+    if !plaid_complete
+      redirect_to plaid_new_path
+    elsif !stripe_complete
+      redirect_to stripe_new_path
+    end
+  end
+
 end

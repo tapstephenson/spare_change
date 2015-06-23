d93af3de00e8ddb2e6a4c7a92abfcd629b9e0bf0
Teresa Martyny <teresamartyny@gmail.com>
2015-06-23 15:46:05 -0700

    make user plural

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,3 +1,3 @@
 class Charity < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :users
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,3 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :users
 end

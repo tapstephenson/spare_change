276d6170de7d7bf7f988aa2f44bacfba596a7a3e
Teresa Martyny <teresamartyny@gmail.com>
2015-06-23 15:44:52 -0700

    remove stash notes

--- app/controllers/concerns/users_concerns.rb
+++ app/controllers/concerns/users_concerns.rb
@@ -5,12 +5,9 @@ module UsersConcerns
     def find_user
       User.find(params[:id])
     end
-<<<<<<< Updated upstream:app/controllers/concerns/users.rb
-=======
     def all_users
       User.all
     end
->>>>>>> Stashed changes:app/controllers/concerns/users_concerns.rb
     def authorize_user
       authorize find_user
     end

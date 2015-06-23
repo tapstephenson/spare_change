e56eb7fee8b559874a9c7cba3b190d7fe593048b
Teresa Martyny <teresamartyny@gmail.com>
2015-06-23 16:56:29 -0700

    add profile_complete methods

--- app/controllers/concerns/users_concerns.rb
+++ app/controllers/concerns/users_concerns.rb
@@ -11,5 +11,21 @@ module UsersConcerns
     def authorize_user
       authorize find_user
     end
+    def profile_complete
+      current_user.bank && 
+      current_user.plaid_access_token &&
+      current_user.stripe_customer_id && 
+      current_user.stripe_subscription_id
+    end
+
+    def plaid_complete
+      current_user.bank &&
+      current_user.plaid_access_token
+    end
+
+    def stripe_complete
+      current_user.stripe_customer_id &&
+      current_user.stripe_subscription_id
+    end
   end
 end

d1979c7e848affa1dbd64ad9d8f630c4bbc8bc85
Teresa Martyny <teresamartyny@gmail.com>
2015-06-23 16:20:42 -0700

    remove testing print

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,5 @@
 class VisitorsController < ApplicationController
   def index
-    p current_user
     if current_user
       if current_user.bank && current_user.plaid_access_token &&
         current_user.stripe_customer_id && current_user.stripe_subscription_id

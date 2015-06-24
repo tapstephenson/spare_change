7b9e388fdc9cac5b2ed7ff9376da946f0112ecb7
joshuacroff <joshuacroff@gmail.com>
2015-06-23 20:15:16 -0700

    modified concerns name

---
+++ app/controllers/concerns/user_concerns.rb
@@ -0,0 +1,12 @@
+module UserConcerns
+  extend ActiveSupport::Concern
+
+  included do
+    def find_user
+      @user = User.find(params[:id])
+    end
+    def authorize_user
+      authorize @user
+    end
+  end
+end

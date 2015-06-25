618c553ddf5f615022c55ba807d097b5be1ce984
Teresa Martyny <teresamartyny@gmail.com>
2015-06-24 19:09:10 -0700

    change time to now

--- app/workers/sidetiq_worker.rb
+++ app/workers/sidetiq_worker.rb
@@ -2,7 +2,7 @@ class SidetiqWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { weekly.day(:friday) }
+  recurrence { weekly.day(:thursday).hour_of_day(19).minute_of_hour(10) }
 
   def perform
     UserMailer.delay.weekly_email(current_user.id)

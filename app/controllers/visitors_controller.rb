7bed311130acb3ad263e94e00c4b1fb2ba70375d
Tapley Stephenson <tapley.stephenson@gmail.com>
2015-06-24 19:40:21 -0700

    add calls for donations stream data

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,9 @@ class VisitorsController < ApplicationController
   def index
     if current_user
       if profile_complete
+        @transactions = current_user.transactions.order(date: :desc).limit(20)
+        @charity = Charity.find(current_user.charity_id)
+
         p "profile complete"
         render :loggedin
       else

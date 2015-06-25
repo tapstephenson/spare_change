eafce88123c85fbccebc85e640abe89eb439081a
Teresa Martyny <teresamartyny@gmail.com>
2015-06-24 18:20:01 -0700

    add price information to the seed file to fix bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,12 +64,15 @@ puts 'CREATED ADMIN USER: ' << user.email
 
 # create 1000 transactions in the past year for user
 1000.times do
+  price = Faker::Commerce.price
   Transaction.create(
     user_id: user.id,
     charity_id: user.charity_id,
     transaction_account: nil,
     transaction_id: nil,
-    amount: Faker::Commerce.price,
+    amount: price,
+    rounded_amount: price.ceil,
+    difference: price.ceil - price,
     date: Faker::Date.backward(365),
     name: Faker::Company.name,
     pending: false

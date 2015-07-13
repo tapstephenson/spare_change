3b2f8190cb54bb97895cd20cddf8810629199121
Teresa Martyny <teresamartyny@gmail.com>
2015-07-12 21:43:37 -0700

    comment out curl call in controller. add curl call to method in model

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,2 +1,12 @@
 class Charge < ActiveRecord::Base
+
+  def self.stripe_new_user_data(params)
+    ActiveSupport::JSON.decode(`curl https://api.stripe.com/v1/customers \
+      -u #{Stripe.api_key}: \
+      -d card[number]=#{params['card']['number']} \
+      -d card[exp_month]=#{params['card']['exp_month']} \
+      -d card[exp_year]=#{params['card']['exp_year']} \
+      -d card[cvc]=#{params['card']['cvc']} \
+      -d plan=#{params['frequency']}`)
+  end
 end

9a84370df7a8a5ccb58d7b347ab844f31017221b
Teresa Martyny <teresamartyny@gmail.com>
2015-07-12 20:47:07 -0700

    moved settings related concerns into their own file

--- app/controllers/concerns/user_concerns.rb
+++ app/controllers/concerns/user_concerns.rb
@@ -14,24 +14,5 @@ module UserConcerns
     def authorize_user
       authorize current_user
     end
-    def profile_complete
-      plaid_complete &&
-      stripe_complete &&
-      charity_complete
-    end
-
-    def plaid_complete
-      current_user.bank &&
-      current_user.plaid_access_token
-    end
-
-    def charity_complete
-      current_user.charity_id
-    end
-
-    def stripe_complete
-      current_user.stripe_customer_id &&
-      current_user.stripe_subscription_id
-    end
   end
 end
